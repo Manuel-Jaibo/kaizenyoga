c6ccc45a19daad442e295ca1b1ea71d1e5119028
manuel <mgmontanez92@gmail.com>
2021-12-13 23:01:46 -0600

    ..ssasjh.

--- routes/tienda.js
+++ routes/tienda.js
@@ -15,8 +15,32 @@ app.set('view engine', 'ejs');
     //     console.log('ni pepe')
     // }
 
+var articulos = [
+    {
+        colecciones: ['todos', 'yoga'],
+        titulo: 'La diarrea', 
+        precio: 700,
+        imagen: 'https://cdn.shopify.com/s/files/1/0555/2766/9898/files/bolsa-para-yoga-dos.jpg?v=1639276551',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur libero repellendus, eaque, amet at aut, voluptatum qui dignissimos voluptatem similique officia hic sequi expedita molestias illo dicta unde necessitatibus fugit.',
+        botonAddToCar: 'd',
+    },
+    {
+        colecciones: ['todos', 'yoga'],
+        titulo: 'La caca', 
+        precio: 700,
+        imagen: 'https://cdn.shopify.com/s/files/1/0555/2766/9898/files/bolsa-para-yoga-dos.jpg?v=1639276551',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur libero repellendus, eaque, amet at aut, voluptatum qui dignissimos voluptatem similique officia hic sequi expedita molestias illo dicta unde necessitatibus fugit.',
+        botonAddToCar: 'd',
+    },
+    {
+        colecciones: ['todos', 'yoga'],
+        titulo: 'El vómito', 
+        precio: 700,
+        imagen: 'https://cdn.shopify.com/s/files/1/0555/2766/9898/files/bolsa-para-yoga-dos.jpg?v=1639276551',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur libero repellendus, eaque, amet at aut, voluptatum qui dignissimos voluptatem similique officia hic sequi expedita molestias illo dicta unde necessitatibus fugit.',
+        botonAddToCar: 'd',
+    },
 
-var arosParaPilates = [
     {
         colecciones: ['todos', 'pilates'],
         imagen: 'https://cdn.shopify.com/s/files/1/0555/2766/9898/files/aroUno.jpg?v=1638330383',
@@ -25,25 +49,46 @@ var arosParaPilates = [
         descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur libero repellendus, eaque, amet at aut, voluptatum qui dignissimos voluptatem similique officia hic sequi expedita molestias illo dicta unde necessitatibus fugit.',
         botonAddToCar: 'd',
     },
-];
-
-var bolsasParaYoga = [
     {
         colecciones: ['todos', 'yoga'],
-        imagen: 'https://cdn.shopify.com/s/files/1/0555/2766/9898/files/bolsa-para-yoga-dos.jpg?v=1639276551',
         titulo: 'Bolsas para yoga', 
         precio: 700,
+        imagen: 'https://cdn.shopify.com/s/files/1/0555/2766/9898/files/bolsa-para-yoga-dos.jpg?v=1639276551',
         descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur libero repellendus, eaque, amet at aut, voluptatum qui dignissimos voluptatem similique officia hic sequi expedita molestias illo dicta unde necessitatibus fugit.',
         botonAddToCar: 'd',
     },
+    {
+        colecciones: ['todos', 'yoga'],
+        titulo: 'Bolsas para yoga', 
+        precio: 700,
+        imagen: 'https://cdn.shopify.com/s/files/1/0555/2766/9898/files/bolsa-para-yoga-dos.jpg?v=1639276551',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur libero repellendus, eaque, amet at aut, voluptatum qui dignissimos voluptatem similique officia hic sequi expedita molestias illo dicta unde necessitatibus fugit.',
+        botonAddToCar: 'd',
+    },
+    {
+        colecciones: ['todos', 'yoga'],
+        titulo: 'Bolsas para yoga', 
+        precio: 700,
+        imagen: 'https://cdn.shopify.com/s/files/1/0555/2766/9898/files/bolsa-para-yoga-dos.jpg?v=1639276551',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur libero repellendus, eaque, amet at aut, voluptatum qui dignissimos voluptatem similique officia hic sequi expedita molestias illo dicta unde necessitatibus fugit.',
+        botonAddToCar: 'd',
+    },
+    {
+        colecciones: ['todos', 'yoga'],
+        titulo: 'Bolsas para yoga', 
+        precio: 700,
+        imagen: 'https://cdn.shopify.com/s/files/1/0555/2766/9898/files/bolsa-para-yoga-dos.jpg?v=1639276551',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur libero repellendus, eaque, amet at aut, voluptatum qui dignissimos voluptatem similique officia hic sequi expedita molestias illo dicta unde necessitatibus fugit.',
+        botonAddToCar: 'd',
+    },
+
 ];
 
 
 route.get('/', function (req, res) {
     // const todos = 'aquí se mostrarán los artículos con la clase "todos" ';
     res.render("tienda", 
-    {arosParaPilates: arosParaPilates,
-     bolsasParaYoga: bolsasParaYoga}
+    {articulos: articulos}
     );}
 );
 
--- static/scripts/pop.js
+++ static/scripts/pop.js
@@ -1,25 +1,30 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+const element = document.getElementById('myBtn');
 
-// Get the button that opens the modal
-let btn = document.getElementById("myBtn"); 
+document.addEventListener('click', (e) => {
+  console.log(e.target.tagName)
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  if (e.target && e.target.tagName === 'BUTTON') {
+    const article = document.getElementsByName('La caca');
+    console.log(article)
+    // Get the modal
+    var modal = document.getElementById("myModal");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-}
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-}
+    modal.style.display = "block";
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    }
+
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
   }
-}
+});
