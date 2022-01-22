6731ce941895a7ad519038009ebd1c5251008cfc
Manuel Gonzalez <mgmontanez92@gmail.com>
2022-01-22 13:39:31 -0600

    22 enero iV

--- static/scripts/scriptMenu.js
+++ static/scripts/scriptMenu.js
@@ -1,35 +1,33 @@
-
-
 // Submenú Meditación
 function coleccionMeditacion(e) {
     const cambioMeditacion = document.getElementsByClassName('listaMeditacion')[0];
     cambioMeditacion.style.display = 'block';
-}
+
+
+    if (cambioMeditacion.style.display = 'block'){
+        const clicBow = document.querySelector('.bannerHome, .tienda');
+        clicBow.addEventListener("click", function pepe(e){
+            cambioMeditacion.style.display = 'none';
+        })
+    }
+};
 const clicMeditacionF = document.querySelector('.clicMeditacion');  
-clicMeditacionF.addEventListener("click", coleccionMeditacion, false)
+clicMeditacionF.addEventListener("click", coleccionMeditacion, false);
+
+
 
 
 //Submenú Yoga
 function coleccionYoga(e) {
     const cambioYoga = document.getElementsByClassName('listaYoga')[0];
-    cambioYoga.style.display = 'block';
-        
-    if (cambioYoga.style.display = 'block'){
-        cambioYoga.addEventListener("click", function pepe(e){
-            cambioYoga.style.display = 'none';
-        })
-    }
-}
+    cambioYoga.style.display = 'block';   
+
+        if (cambioYoga.style.display = 'block'){
+            const clicBow = document.querySelector('.bannerHome, .tienda');
+            clicBow.addEventListener("click", function pepe(e){
+                cambioYoga.style.display = 'none';
+            })
+        }
+};
 const clicYogaF = document.querySelector('.clicYoga');  
-clicYogaF.addEventListener("click", coleccionYoga, false)
-
-
-
-// function pepe(e){
-//     const cambioYoga = document.getElementsByClassName('listaYoga')[0];
-//     cambioYoga.style.display = 'none';
-//     const cambioMeditacion = document.getElementsByClassName('listaMeditacion')[0];
-//     cambioMeditacion.style.display = 'none';
-// }
-// const clicBow = document.querySelector('.bannerHome');
-// clicBow.addEventListener("click", pepe, false)
+clicYogaF.addEventListener("click", coleccionYoga, false);
