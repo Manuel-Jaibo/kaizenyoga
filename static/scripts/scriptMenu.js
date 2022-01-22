263f8e346c5999740407b573951a701bb9ba6272
Manuel Gonzalez <mgmontanez92@gmail.com>
2022-01-22 10:03:17 -0600

    22 enero 2022 II

--- static/scripts/scriptMenu.js
+++ static/scripts/scriptMenu.js
@@ -1,9 +1,35 @@
-function obtenerMenuMeditacion(){
-    const medintacionMenuu  = document.getElementsByClassName('menuMeditacion');
-    medintacionMenuu.style.display = "flex";
+
+
+// Submenú Meditación
+function coleccionMeditacion(e) {
+    const cambioMeditacion = document.getElementsByClassName('listaMeditacion')[0];
+    cambioMeditacion.style.display = 'block';
 }
+const clicMeditacionF = document.querySelector('.clicMeditacion');  
+clicMeditacionF.addEventListener("click", coleccionMeditacion, false)
 
 
-const meditacionMenu  = document.getElementsByClassName('menuMeditacion');
-meditacionMenu.addEventListener("click", obtenerMenuMeditacion, false);
+//Submenú Yoga
+function coleccionYoga(e) {
+    const cambioYoga = document.getElementsByClassName('listaYoga')[0];
+    cambioYoga.style.display = 'block';
+        
+    if (cambioYoga.style.display = 'block'){
+        cambioYoga.addEventListener("click", function pepe(e){
+            cambioYoga.style.display = 'none';
+        })
+    }
+}
+const clicYogaF = document.querySelector('.clicYoga');  
+clicYogaF.addEventListener("click", coleccionYoga, false)
 
+
+
+// function pepe(e){
+//     const cambioYoga = document.getElementsByClassName('listaYoga')[0];
+//     cambioYoga.style.display = 'none';
+//     const cambioMeditacion = document.getElementsByClassName('listaMeditacion')[0];
+//     cambioMeditacion.style.display = 'none';
+// }
+// const clicBow = document.querySelector('.bannerHome');
+// clicBow.addEventListener("click", pepe, false)
